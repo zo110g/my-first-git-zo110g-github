5ab7bfa40cf708a40ff643b931a3f455262c1272
zacha <zacha@DESKTOP-E0KTUNR.attlocal.net>
2017-01-20 08:46:35 -0600

    Adding my name to HelloGit

--- my-first-git-zo110g/src/com/att/cicd/HelloGit.java
+++ my-first-git-zo110g/src/com/att/cicd/HelloGit.java
@@ -3,6 +3,6 @@ package com.att.cicd;
 public class HelloGit {
      
     public static void main(String[] args) {
-        System.out.println("Hello Your Name! This is my first sample git project.");
+        System.out.println("Hello Zachary O'Brien! This is my first sample git project.");
     }
 }
