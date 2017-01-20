c00a1383bdeb9caa3acd333850c7c6356e3fc3a1
zacha <zacha@DESKTOP-E0KTUNR.attlocal.net>
2017-01-20 08:45:12 -0600

    Add new file for Hello application

---
+++ my-first-git-zo110g/src/com/att/cicd/HelloGit.java
@@ -0,0 +1,8 @@
+package com.att.cicd;
+ 
+public class HelloGit {
+     
+    public static void main(String[] args) {
+        System.out.println("Hello Your Name! This is my first sample git project.");
+    }
+}
